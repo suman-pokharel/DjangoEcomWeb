907fea9919d1714814cd74a2b837b8657347a8d9
suman pokharel <sumanpokharel8@gmail.com>
2022-08-11 02:20:46 +0545

    edit

--- Ecom/urls.py
+++ Ecom/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('', include('home.api_urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-] +static(settings.MEDIA_URL,
-                           document_root=settings.MEDIA_ROOT) +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
 # # if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL,
+                           document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
