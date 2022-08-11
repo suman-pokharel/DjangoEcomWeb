77bc041e113d0cc5097023ca96870a6bd8a11d34
suman pokharel <sumanpokharel8@gmail.com>
2022-08-11 18:18:19 +0545

    testing

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR,'static/']
-STATIC_ROOT=os.path.join(BASE_DIR,'/static/')
+STATIC_ROOT=[BASE_DIR,'static']
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
