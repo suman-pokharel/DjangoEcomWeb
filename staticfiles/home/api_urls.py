fdfa1ef1d862625650b495aeda9503b8b81df39e
suman pokharel <sumanpokharel8@gmail.com>
2022-08-11 20:29:55 +0545

    final project

---
+++ staticfiles/home/api_urls.py
@@ -0,0 +1,15 @@
+from .api_views import *
+from django.urls import path, include
+from rest_framework import routers
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'products', ProductViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    
+]
