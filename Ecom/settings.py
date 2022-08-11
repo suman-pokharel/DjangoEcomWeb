25278d237d05d1ce10d1b6375368c09b5c6714d8
suman pokharel <sumanpokharel8@gmail.com>
2022-08-11 20:26:31 +0545

    final project

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS=[BASE_DIR,'static/']
-STATIC_ROOT=os.path.join(BASE_DIR,'/staticfiles/')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles/')
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
