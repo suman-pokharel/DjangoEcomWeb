f17ffc1ba72173f0ee1443e9c27e9ab96bf635ee
suman pokharel <sumanpokharel8@gmail.com>
2022-08-20 11:12:34 +0545

    api Filters

--- home/api_views.py
+++ home/api_views.py
@@ -1,12 +1,14 @@
 from .models import *
 from .serializers import *
 from rest_framework import  viewsets
-from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+import django_filters.rest_framework
+from rest_framework import generics
 
 
 
-class ProductList(generics.ListAPIView):
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # filter_backends=[DjangoFilterBackend]
