b31bd4d949322b172a81b042e70f901a4fad48e8
suman pokharel <sumanpokharel8@gmail.com>
2022-08-20 10:10:30 +0545

    api Filters

--- home/api_views.py
+++ home/api_views.py
@@ -7,7 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 
-class ProductViewSet(ListAPIView):
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends=[DjangoFilterBackend]
